176d193a6d634d34465aa5618b4fbbbb03242853
MagaJL <48716048@est.ort.edu.ar>
2025-04-21 10:41:27 -0300

    Saludo terminado

---
+++ Assets/uimanager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class uimanager : MonoBehaviour
+{
+    public TextMeshProUGUI txtSaludo;
+    public TMP_InputField inputNombre;
+    string nombreIngresado;
+    string saludo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtSaludo.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Sludar()
+    {
+        // Obtener el nombre del inputfild
+
+        nombreIngresado = inputNombre.text;
+
+        // Concatenar el nombre con saludo
+
+        saludo = "Hola " + nombreIngresado + "!";
+
+        // Mostrar el saludo en txtSaludo
+        txtSaludo.text = saludo;
+    }
+}
