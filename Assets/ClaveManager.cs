4d846e8732a06715ae8e2aa65ce8a9e58ddc5c27
MagaJL <48716048@est.ort.edu.ar>
2025-05-05 10:29:16 -0300

    EAC terminado

---
+++ Assets/ClaveManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ClaveManager : MonoBehaviour
+{
+    public TextMeshProUGUI txtResultado;
+    public TMP_InputField inputClave;
+    string claveIngresada;
+    string clave;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtResultado.text = "";
+        clave = "amorypaz";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Clave()
+    {
+        // Obtener lo ingresado del inputfild
+
+        claveIngresada = inputClave.text;
+
+        // Chequear la clave
+
+        if (claveIngresada == clave)
+        {
+            txtResultado.text = "La clave ingresada es correcta";
+        }
+        else
+        {
+            txtResultado.text = "La clave ingresada es incorrecta";
+        }
+
+        // Mostrar el estado de la clave
+    }
+}
